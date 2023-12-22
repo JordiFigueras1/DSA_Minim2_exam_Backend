0a296ed850bd61b25e2e6d082b2eeebcab4010f8
JordiFigueras1 <jfg56734@gmail.com>
2023-12-22 08:29:16 +0100

    Commit plantilla Backend Minim 2

---
+++ src/main/java/edu/upc/dsa/JuegoManager.java
@@ -0,0 +1,20 @@
+package edu.upc.dsa;
+
+import edu.upc.dsa.models.Usuario;
+import edu.upc.dsa.models.VOCredenciales;
+
+import java.util.ArrayList;
+import java.util.List;
+public interface JuegoManager {
+    public int addUsuario(String username, String mail, String name, String lastName, String password);
+    public int registrarUsuario(Usuario u);
+    public Usuario login(VOCredenciales credencialesu);
+    public VOCredenciales getCredenciales(Usuario U);
+    public Usuario getUser(String mail);
+    public List<Usuario> getallusers();
+    public int sizeUsers();
+    public int deleteUsuario(VOCredenciales credenciales);
+
+    public Usuario actualizarUsuario(String mail, String newUsername, String newName, String newLastName, String newPassword, String newMail);
+
+    }
